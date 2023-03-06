89ee2944c890e85552ffd812ea128ecc697d643f
Abdulaziz Alsiedi <S210036054@seu.edu.sa>
2023-03-06 06:26:26 +0300

    Player assets

--- The-Elites/Assets/Scripts/PlayerMovement.cs
+++ The-Elites/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D RBplayer;
     private float Direction;
     [SerializeField]private float MovementSpeed = 7f;
+    [SerializeField]private float JumpPower = 8f;
     void Start()
     {
         RBplayer = GetComponent<Rigidbody2D>();
@@ -19,5 +20,13 @@ public class PlayerMovement : MonoBehaviour
     {
         Direction = Input.GetAxisRaw("Horizontal");
         RBplayer.velocity = new Vector2(Direction * MovementSpeed, RBplayer.velocity.y);
+
+        // Jumping of the player
+        if (Input.GetButtonDown("Jump"))
+        {
+            RBplayer.velocity = new Vector2(RBplayer.velocity.x, JumpPower);
+        }
     }
+
+
 }
