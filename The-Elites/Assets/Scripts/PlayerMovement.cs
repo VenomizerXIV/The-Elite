7b492034bff3c2f3e0d2e382551556edeb484ea7
Abdulaziz Alsiedi <S210036054@seu.edu.sa>
2023-03-06 09:59:18 +0300

    player animation update
    player animation , running , idle , jump , fall

--- The-Elites/Assets/Scripts/PlayerMovement.cs
+++ The-Elites/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private enum PlayerMtype
+    {
+        idle , Running , Jump , Fall
+    }
+    private SpriteRenderer sprite;
+    private Animator anim;
     private Rigidbody2D RBplayer;
     private float Direction;
     [SerializeField]private float MovementSpeed = 7f;
@@ -11,7 +17,8 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         RBplayer = GetComponent<Rigidbody2D>();
-        
+        anim =  GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     
@@ -26,7 +33,37 @@ public class PlayerMovement : MonoBehaviour
         {
             RBplayer.velocity = new Vector2(RBplayer.velocity.x, JumpPower);
         }
+        UpdateAnimation();
     }
+    // Animation of the player
+    private void UpdateAnimation()
+    {
+        PlayerMtype State;
+        if (Direction > 0f)
+        {
+            State = PlayerMtype.Running;
+            sprite.flipX = false;
+        }
+        else if (Direction < 0f)
+        {
+            State = PlayerMtype.Running;
+            sprite.flipX = true;
 
+        }
+        else
+        {
+            State = PlayerMtype.idle;
+        }
+        if (RBplayer.velocity.y > 0.1f)
+        {
+            State = PlayerMtype.Jump;
+        }
+        else if (RBplayer.velocity.y < -0.1f)
+        {
+            State = PlayerMtype.Fall;
+        }
+        
+        anim.SetInteger("state", (int)State);
 
+}
 }
